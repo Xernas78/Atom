73876b5bc6280e272f33991722dd22d95301403f
Xernas78 <71571183+Xernas78@users.noreply.github.com>
2025-06-20 00:43:25 +0200

    Added average method

---
+++ src/main/java/dev/xernas/atom/math/MathUtils.java
@@ -0,0 +1,16 @@
+package dev.xernas.atom.math;
+
+public class MathUtils {
+
+    public static double average(double... values) {
+        if (values.length == 0) {
+            return 0.0;
+        }
+        double sum = 0.0;
+        for (double value : values) {
+            sum += value;
+        }
+        return sum / values.length;
+    }
+
+}
